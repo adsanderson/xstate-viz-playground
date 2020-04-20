76191b031fb0fdf50a114c727080069f55d18c07
Adam Sanderson <adam@adamsanderson.co.uk>
2020-04-20 11:15:42 +0100

    extract component and pass in service

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 import "./styles.css";
 import { MachineViz } from "@xstate/viz";
-import { useMachine } from "@xstate/react";
+import { useMachine, useService } from "@xstate/react";
 import { createMachine, State, interpret } from "xstate";
 
 import "@xstate/viz/themes/dark.css";
@@ -16,12 +16,23 @@ const toggleMachine = createMachine({
 });
 
 export default function App() {
-  const [current, send] = useMachine(toggleMachine);
+  const [current, send, service] = useMachine(toggleMachine);
   return (
     <main className="App">
       <h1>XState vix test</h1>
-      <button onClick={() => send("TOGGLE")}>Toggle</button>
+      <Toggle service={service} />
       <MachineViz machine={toggleMachine} state={current} />
     </main>
+  );
+}
+
+function Toggle(props: { service: any }) {
+  const [currentToggle, send] = useService(props.service);
+  return (
+    <>
+      {currentToggle.matches("inactive") && <h1>Off</h1>}
+      {currentToggle.matches("active") && <h1>On</h1>}
+      <button onClick={() => send("TOGGLE")}>Toggle</button>
+    </>
   );
 }
